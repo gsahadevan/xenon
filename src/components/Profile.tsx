56c6ffd481e57147851014a50605f336fe81b95f
Gauthaman Sahadevan <gsahadevan@outlook.com>
2022-12-09 18:12:16 +0100

    fix: show dropdowns by adding background white

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -21,7 +21,7 @@ export const Profile = (): JSX.Element => {
                     leave='transition ease-in duration-75'
                     leaveFrom='transform opacity-100 scale-100'
                     leaveTo='transform opacity-0 scale-95'>
-                    <Menu.Items className='origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 focus:outline-none'>
+                    <Menu.Items className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
                         <Menu.Item>
                             <a href='/dashboard' className='block px-2 py-1 text-sm'>
                                 <Link to='/error'>
--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Messages } from '../components/Messages';
 import { Notifications } from '../components/Notifications';
 import { Profile } from '../components/Profile';
+import { Bars3Icon } from '@heroicons/react/24/outline';
 
 export const Header = (): JSX.Element => {
+    const [sidebar, setSidebar] = useState(false);
+
+    useEffect(() => {
+        console.log(sidebar);
+    }, [sidebar]);
+
     return (
-        <header className='z-40 py-4 shadow-lg'>
+        <header className='py-4 shadow-lg'>
             <div className='container flex items-center lg:justify-end justify-between h-full md:px-12 px-4 mx-auto'>
                 <button
                     className='p-1 mr-5 -ml-1 rounded-md lg:hidden focus:outline-none focus:shadow-outline'
-                    aria-label='Menu'>
-                    Example
+                    aria-label='Menu'
+                    onClick={() => setSidebar(!sidebar)}>
+                    <Bars3Icon className='w-6 h-6' aria-hidden='true' />
                 </button>
 
                 <div className='flex items-center pr-2 space-x-4'>
