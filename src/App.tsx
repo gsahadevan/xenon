0b218d5595cb1d8caabff95d4444e4114d79b480
Gauthaman Sahadevan <gsahadevan@outlook.com>
2022-12-09 12:04:02 +0100

    feat: add basic layout
    - add header
    - add sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Error from "./pages/Error";
+import DefaultLayout from "./layouts/DefaultLayout";
 
 function App() {
   // return <h1>I am just tryign out stuffs</h1>;
 
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />}></Route>
-      <Route path="/error" element={<Error />}></Route>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route index element={<Dashboard />}></Route>
+        <Route path="dashboard" element={<Dashboard />}></Route>
+        <Route path="error" element={<Error />}></Route>
+      </Route>
     </Routes>
   );
 }
---
+++ src/layouts/DefaultLayout.js
@@ -0,0 +1,21 @@
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+
+function Layout() {
+  return (
+    <div className="flex h-screen bg-gray-50 dark:bg-gray-900">
+      <Sidebar />
+      <div className="flex flex-col flex-1 w-full">
+        <Header />
+        <main className="h-full pb-16 overflow-y-auto">
+          <div className="container grid md:px-12 px-4 mx-auto">
+            <Outlet />
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
