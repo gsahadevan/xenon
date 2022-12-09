e0b7813686823dd3517b64975b670a67c644e30d
Gauthaman Sahadevan <gsahadevan@outlook.com>
2022-12-09 12:28:58 +0100

    style: extend prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,15 @@ import Error from './pages/Error';
 import DefaultLayout from './layouts/DefaultLayout';
 
 function App() {
-  // return <h1>I am just tryign out stuffs</h1>;
-
-  return (
-    <Routes>
-      <Route path='/' element={<DefaultLayout />}>
-        <Route index element={<Dashboard />}></Route>
-        <Route path='dashboard' element={<Dashboard />}></Route>
-        <Route path='error' element={<Error />}></Route>
-      </Route>
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path='/' element={<DefaultLayout />}>
+                <Route index element={<Dashboard />}></Route>
+                <Route path='dashboard' element={<Dashboard />}></Route>
+                <Route path='error' element={<Error />}></Route>
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
