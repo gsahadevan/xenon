9050ae2289d5a4c7ccc08c7c18e00538455d64c2
Gauthaman Sahadevan <gsahadevan@outlook.com>
2022-12-09 12:10:57 +0100

    style: add prettier support

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 // import React from "react";
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import Error from "./pages/Error";
-import DefaultLayout from "./layouts/DefaultLayout";
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Error from './pages/Error';
+import DefaultLayout from './layouts/DefaultLayout';
 
 function App() {
   // return <h1>I am just tryign out stuffs</h1>;
 
   return (
     <Routes>
-      <Route path="/" element={<DefaultLayout />}>
+      <Route path='/' element={<DefaultLayout />}>
         <Route index element={<Dashboard />}></Route>
-        <Route path="dashboard" element={<Dashboard />}></Route>
-        <Route path="error" element={<Error />}></Route>
+        <Route path='dashboard' element={<Dashboard />}></Route>
+        <Route path='error' element={<Error />}></Route>
       </Route>
     </Routes>
   );
