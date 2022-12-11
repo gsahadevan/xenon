8793e41dddc07ce6a2c3bc89c986c239d66405cf
Gauthaman Sahadevan <gsahadevan@outlook.com>
2022-12-11 17:48:30 +0100

    fix: sidebar panel closes when clicked on body
    - installed @tailwindcss/aspect-ratio

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -1,7 +1,34 @@
 import { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Dialog, Transition } from '@headlessui/react';
-import { Bars3Icon, ChevronLeftIcon, XMarkIcon } from '@heroicons/react/24/outline/';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline/';
+
+const sidebarMenu = [
+    {
+        link: '/dashboard',
+        text: 'Dashboard',
+    },
+    {
+        link: '/error',
+        text: 'Error',
+    },
+    {
+        link: '/cta',
+        text: 'CTA Samples',
+    },
+    {
+        link: '/banner',
+        text: 'Header Banner',
+    },
+    {
+        link: '/layout',
+        text: 'Layout',
+    },
+    {
+        link: '/navbars',
+        text: 'Navbars',
+    },
+];
 
 export const Sidebar = (): JSX.Element => {
     const [open, setOpen] = useState(false);
@@ -16,95 +43,58 @@ export const Sidebar = (): JSX.Element => {
                 <Bars3Icon className='w-5 h-5 text-white' />
             </div>
             <Transition.Root show={open} as={Fragment}>
-                <Dialog as='div' className='fixed inset-0 overflow-hidden z-9999' onClose={() => setOpen(false)}>
-                    <div className='absolute inset-0 overflow-hidden'>
+                <Dialog as='div' className='relative z-40' onClose={setOpen}>
+                    <Transition.Child
+                        as={Fragment}
+                        enter='transition-opacity ease-linear duration-300'
+                        enterFrom='opacity-0'
+                        enterTo='opacity-100'
+                        leave='transition-opacity ease-linear duration-300'
+                        leaveFrom='opacity-100'
+                        leaveTo='opacity-0'>
+                        <div className='fixed inset-0 bg-black bg-opacity-25' />
+                    </Transition.Child>
+
+                    <div className='fixed inset-0 z-40 flex'>
                         <Transition.Child
                             as={Fragment}
-                            enter='ease-in-out duration-500'
-                            enterFrom='opacity-0'
-                            enterTo='opacity-100'
-                            leave='ease-in-out duration-500'
-                            leaveFrom='opacity-100'
-                            leaveTo='opacity-0'>
-                            <Dialog.Overlay className='absolute inset-0 bg-opacity-75 transition-opacity' />
-                        </Transition.Child>
-                        <div className='fixed inset-0 left-0 pr-10 max-w-full flex'>
-                            <Transition.Child
-                                as={Fragment}
-                                enter='transform transition ease-in-out duration-500 sm:duration-700'
-                                enterFrom='-translate-x-full'
-                                enterTo='translate-x-0'
-                                leave='transform transition ease-in-out duration-500 sm:duration-700'
-                                leaveFrom='translate-x-0'
-                                leaveTo='-translate-x-full'>
-                                <div className='relative w-screen max-w-md'>
-                                    <div className='h-full flex flex-col py-6 bg-white shadow-xl overflow-y-scroll'>
-                                        <div className='px-4 sm:px-6'>
-                                            <div className='flex justify-between mb-4'>
-                                                <Dialog.Title className='text-lg font-medium'>Xenon</Dialog.Title>
-                                                <button
-                                                    className='text-gray-900 focus:outline-none px-2 py-1'
-                                                    onClick={() => {
-                                                        setOpen(open => !open);
-                                                    }}>
-                                                    <XMarkIcon className='h-6 w-6' aria-hidden='true' />
-                                                    <span className='sr-only'>Close panel</span>
-                                                </button>
-                                            </div>
-                                            <hr />
-                                        </div>
-                                        <div className='mt-6 relative flex-1 px-4 sm:px-6'>
-                                            {/* Replace with your content */}
-                                            <ul>
-                                                <li className='items-center'>
-                                                    <Link to='/dashboard'>
-                                                        <div className='text-sm px-4 py-2 flex items-center hover:bg-gray-100 rounded'>
-                                                            <span>Dashboard</span>
-                                                        </div>
-                                                    </Link>
-                                                </li>
-                                                <li className='items-center'>
-                                                    <Link to='/error'>
-                                                        <div className='text-sm px-4 py-2 flex items-center hover:bg-gray-100 rounded'>
-                                                            <span>Error</span>
-                                                        </div>
-                                                    </Link>
-                                                </li>
-                                                <li className='items-center'>
-                                                    <Link to='/cta'>
-                                                        <div className='text-sm px-4 py-2 flex items-center hover:bg-gray-100 rounded'>
-                                                            <span>CTA Samples</span>
-                                                        </div>
-                                                    </Link>
-                                                </li>
-                                                <li className='items-center'>
-                                                    <Link to='/banner'>
-                                                        <div className='text-sm px-4 py-2 flex items-center hover:bg-gray-100 rounded'>
-                                                            <span>Header Banner</span>
-                                                        </div>
-                                                    </Link>
-                                                </li>
-                                                <li className='items-center'>
-                                                    <Link to='/layout'>
-                                                        <div className='text-sm px-4 py-2 flex items-center hover:bg-gray-100 rounded'>
-                                                            <span>Layout</span>
-                                                        </div>
-                                                    </Link>
-                                                </li>
-                                                <li className='items-center'>
-                                                    <Link to='/navbars'>
-                                                        <div className='text-sm px-4 py-2 flex items-center hover:bg-gray-100 rounded'>
-                                                            <span>Navbars</span>
-                                                        </div>
-                                                    </Link>
-                                                </li>
-                                            </ul>
-                                            {/* /End replace */}
-                                        </div>
+                            enter='transition ease-in-out duration-300 transform'
+                            enterFrom='-translate-x-full'
+                            enterTo='translate-x-0'
+                            leave='transition ease-in-out duration-300 transform'
+                            leaveFrom='translate-x-0'
+                            leaveTo='-translate-x-full'>
+                            <Dialog.Panel className='relative flex w-full max-w-xs flex-col overflow-y-auto bg-white pb-12 shadow-xl'>
+                                <div className='px-4 sm:px-6 py-6'>
+                                    <div className='flex justify-between mb-4'>
+                                        <Dialog.Title className='text-lg font-medium'>Xenon</Dialog.Title>
+                                        <button
+                                            className='text-gray-900 focus:outline-none px-2 py-1'
+                                            onClick={() => {
+                                                setOpen(open => !open);
+                                            }}>
+                                            <XMarkIcon className='h-6 w-6' aria-hidden='true' />
+                                            <span className='sr-only'>Close panel</span>
+                                        </button>
                                     </div>
+                                    <hr />
                                 </div>
-                            </Transition.Child>
-                        </div>
+                                <div className='mt-6 relative flex-1 px-4 sm:px-6'>
+                                    <ul>
+                                        {sidebarMenu.map(menu => (
+                                            <li className='items-center'>
+                                                <Link to={menu.link}>
+                                                    <div className='text-sm px-4 py-2 flex items-center hover:bg-gray-100 rounded'>
+                                                        <span>{menu.text}</span>
+                                                    </div>
+                                                </Link>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                    {/* /End replace */}
+                                </div>
+                            </Dialog.Panel>
+                        </Transition.Child>
                     </div>
                 </Dialog>
             </Transition.Root>
